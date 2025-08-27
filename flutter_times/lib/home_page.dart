58bcec1e6b2864d14c4e84b575d6fbb49816d3cb
milenaabernardi <milenaabernardi@gmail.com>
2025-08-27 09:44:00 -0300

    27/08/2025

---
+++ flutter_times/lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
